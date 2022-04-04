c3dd9f186dde1be5ba7cfcb0ede1454586c0e13b
lhchavez <lhchavez@lhchavez.com>
2022-04-04 06:45:40 -0700

    Evict the omegaup-specific Python packages (#33)
    Currently we need to make a change to this package every single time we
    add a new dependency on the omegaup repository. That's a huge drag.
    
    This change expects there to be a virtual environment in the omegaup
    repository, so that all the Python tools can be managed over there.

--- git_tools.py
+++ git_tools.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 '''
 Utility functions used to write git hooks.
--- test/uppercase_linter.py
+++ test/uppercase_linter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """Trivial linter that converts to uppercase."""
 
